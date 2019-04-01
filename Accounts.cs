0108a688289f67da4619f3968b86147a17aad8ec
Andrew Kelly <36462830+AndrewKelly24@users.noreply.github.com>
2019-04-02 09:48:06 +1100

    Create Accounts.cs

---
+++ Accounts.cs
@@ -0,0 +1,52 @@
+using System;
+
+//Creating the Account Class
+public class Account
+{
+    private decimal _balance;
+
+    private string _name;
+
+    //Method used to initilise an account object
+    public Account(string name, decimal startingBalance)
+    {
+        _name = name;
+        _balance = startingBalance;
+    }
+
+    //Method for adding funds into an account
+    public bool Deposit(decimal amountToAdd)
+    {
+        if (amountToAdd > 0)
+        {
+            _balance += amountToAdd;
+            return true;
+        }
+        return false;
+    }
+
+    //Method for withdrawing funds from an account
+    public bool Withdraw(decimal amountToSubtract)
+    {
+        if (amountToSubtract > 0 && _balance >= amountToSubtract)
+        {
+            _balance -= amountToSubtract;
+            return true;
+        }
+        return false;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    //Print account details
+    public void PrintAccount()
+    {
+
+        Console.WriteLine("Account Name: " + _name);
+        Console.WriteLine("Account Balance: $" + _balance);
+
+    }
+}
